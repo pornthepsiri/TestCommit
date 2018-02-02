8bee554b6de3389abbc1e5cf47edeba3f263a09c
581097 <581097@HQ8503581097N1.KTB.KTBDOMAIN>
2018-02-02 14:24:34 +0700

    1

--- TestGit/src/Main.java
+++ TestGit/src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("TestGit 5"); 
+		System.out.println("TestGit 5");
+		//...Commit 13:30
 	}
 
 }
